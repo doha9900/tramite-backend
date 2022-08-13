002fbc79dc6ab3611b793c08238d28d195ffb15c
doha9900 <dhuarez.arevalo@gmail.com>
2022-08-12 22:48:48 -0500

    new service SUNAT API

--- controllers/usuarios.js
+++ controllers/usuarios.js
@@ -1,194 +1,185 @@
-const { response } = require('express');
-const bcrypt = require('bcryptjs');
+const { response } = require("express");
+const bcrypt = require("bcryptjs");
 
-const Usuario = require('../models/usuario');
-const { generarJWT } = require('../helpers/jwt');
+const Usuario = require("../models/usuario");
+const { generarJWT } = require("../helpers/jwt");
+const axios = require("axios");
 
+const getUsuarios = async (req, res) => {
+  const desde = Number(req.query.desde) || 0;
 
-const getUsuarios = async(req, res) => {
+  const [usuarios, total] = await Promise.all([
+    Usuario.find({}, "nombre email role google img").skip(desde).limit(100),
+    Usuario.countDocuments(),
+  ]);
 
-    const desde = Number(req.query.desde) || 0;
+  res.json({
+    ok: true,
+    usuarios,
+    total,
+  });
+};
 
-    const [ usuarios, total ] = await Promise.all([
-        Usuario
-            .find({}, 'nombre email role google img')
-            .skip( desde )
-            .limit( 100 ),
-        Usuario.countDocuments()
-    ]);
+const miUsuarioDetalles = async (req, res) => {
+  const id = req.params.id;
+  const me = await Usuario.findById(id);
+  res.json({
+    ok: true,
+    usuario: me,
+  });
+};
 
-
-    res.json({
+const miUsuarioSUNATApi = async (req, res) => {
+  const id = req.params.id;
+  const me = await Usuario.findById(id);
+  axios.defaults.headers = {
+    "Content-Type": "application/json",
+    Authorization: "Bearer " + process.env.APIS_NET_TOKEN,
+  };
+  axios
+    .get("https://api.apis.net.pe/v1/ruc?numero=" + me.RUC)
+    .then(async function (response) {
+      res.json({
         ok: true,
-        usuarios,
-        total
+        message: response.data,
+      });
+    })
+    .catch(function (error) {
+      // handle error
+      console.log("No se encontraron resultados", error);
+      res.json({
+        ok: false,
+        message: "RUC Inválido.",
+      });
     });
+};
 
-}
+const crearUsuario = async (req, res = response) => {
+  const { email, password } = req.body;
 
-const miUsuarioDetalles = async(req, res) => {
+  try {
+    const existeEmail = await Usuario.findOne({ email });
 
-    const id = req.params.id;
-    console.log(id);
-    const me = await Usuario.findById(id);
-    res.json({
-        ok: true,
-        "usuario": me,
-        "reniec_api_data": {} //TODO: RENIEC API
-    });
-
-}
-
-const crearUsuario = async(req, res = response) => {
-
-    const { email, password } = req.body;
-
-    try {
-
-        const existeEmail = await Usuario.findOne({ email });
-
-        if ( existeEmail ) {
-            return res.status(400).json({
-                ok: false,
-                msg: 'El correo ya está registrado'
-            });
-        }
-
-        const usuario = new Usuario( req.body );
-    
-        // Encriptar contraseña
-        const salt = bcrypt.genSaltSync();
-        usuario.password = bcrypt.hashSync( password, salt );
-    
-    
-        // Guardar usuario
-        await usuario.save();
-
-        // Generar el TOKEN - JWT
-        const token = await generarJWT( usuario.id );
-
-
-        res.json({
-            ok: true,
-            usuario,
-            token
-        });
-
-
-    } catch (error) {
-        console.log(error);
-        res.status(500).json({
-            ok: false,
-            msg: 'Error inesperado... revisar logs'
-        });
+    if (existeEmail) {
+      return res.status(400).json({
+        ok: false,
+        msg: "El correo ya está registrado",
+      });
     }
 
+    const usuario = new Usuario(req.body);
 
-}
+    // Encriptar contraseña
+    const salt = bcrypt.genSaltSync();
+    usuario.password = bcrypt.hashSync(password, salt);
 
+    // Guardar usuario
+    await usuario.save();
+
+    // Generar el TOKEN - JWT
+    const token = await generarJWT(usuario.id);
+
+    res.json({
+      ok: true,
+      usuario,
+      token,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      ok: false,
+      msg: "Error inesperado... revisar logs",
+    });
+  }
+};
 
 const actualizarUsuario = async (req, res = response) => {
+  // TODO: Validar token y comprobar si es el usuario correcto
 
-    // TODO: Validar token y comprobar si es el usuario correcto
+  const uid = req.params.id;
 
-    const uid = req.params.id;
+  try {
+    const usuarioDB = await Usuario.findById(uid);
 
-
-    try {
-
-        const usuarioDB = await Usuario.findById( uid );
-
-        if ( !usuarioDB ) {
-            return res.status(404).json({
-                ok: false,
-                msg: 'No existe un usuario por ese id'
-            });
-        }
-
-        // Actualizaciones
-        const { password, google, email, ...campos } = req.body;
-
-        if ( usuarioDB.email !== email ) {
-
-            const existeEmail = await Usuario.findOne({ email });
-            if ( existeEmail ) {
-                return res.status(400).json({
-                    ok: false,
-                    msg: 'Ya existe un usuario con ese email'
-                });
-            }
-        }
-        
-        if ( !usuarioDB.google ){
-            campos.email = email;
-        } else if ( usuarioDB.email !== email ) {
-            return res.status(400).json({
-                ok: false,
-                msg: 'Usuario de google no pueden cambiar su correo'
-            });
-        }
-
-        const usuarioActualizado = await Usuario.findByIdAndUpdate( uid, campos, { new: true } );
-
-        res.json({
-            ok: true,
-            usuario: usuarioActualizado
-        });
-
-        
-    } catch (error) {
-        console.log(error);
-        res.status(500).json({
-            ok: false,
-            msg: 'Error inesperado'
-        })
+    if (!usuarioDB) {
+      return res.status(404).json({
+        ok: false,
+        msg: "No existe un usuario por ese id",
+      });
     }
 
-}
+    // Actualizaciones
+    const { password, google, email, ...campos } = req.body;
 
-
-const borrarUsuario = async(req, res = response ) => {
-
-    const uid = req.params.id;
-
-    try {
-
-        const usuarioDB = await Usuario.findById( uid );
-
-        if ( !usuarioDB ) {
-            return res.status(404).json({
-                ok: false,
-                msg: 'No existe un usuario por ese id'
-            });
-        }
-
-        await Usuario.findByIdAndDelete( uid );
-
-        
-        res.json({
-            ok: true,
-            msg: 'Usuario eliminado'
+    if (usuarioDB.email !== email) {
+      const existeEmail = await Usuario.findOne({ email });
+      if (existeEmail) {
+        return res.status(400).json({
+          ok: false,
+          msg: "Ya existe un usuario con ese email",
         });
-
-    } catch (error) {
-        
-        console.log(error);
-        res.status(500).json({
-            ok: false,
-            msg: 'Hable con el administrador'
-        });
-
+      }
     }
 
+    if (!usuarioDB.google) {
+      campos.email = email;
+    } else if (usuarioDB.email !== email) {
+      return res.status(400).json({
+        ok: false,
+        msg: "Usuario de google no pueden cambiar su correo",
+      });
+    }
 
-}
+    const usuarioActualizado = await Usuario.findByIdAndUpdate(uid, campos, {
+      new: true,
+    });
 
+    res.json({
+      ok: true,
+      usuario: usuarioActualizado,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      ok: false,
+      msg: "Error inesperado",
+    });
+  }
+};
 
+const borrarUsuario = async (req, res = response) => {
+  const uid = req.params.id;
+
+  try {
+    const usuarioDB = await Usuario.findById(uid);
+
+    if (!usuarioDB) {
+      return res.status(404).json({
+        ok: false,
+        msg: "No existe un usuario por ese id",
+      });
+    }
+
+    await Usuario.findByIdAndDelete(uid);
+
+    res.json({
+      ok: true,
+      msg: "Usuario eliminado",
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      ok: false,
+      msg: "Hable con el administrador",
+    });
+  }
+};
 
 module.exports = {
-    getUsuarios,
-    miUsuarioDetalles,
-    crearUsuario,
-    actualizarUsuario,
-    borrarUsuario
-}
+  getUsuarios,
+  miUsuarioDetalles,
+  miUsuarioSUNATApi,
+  crearUsuario,
+  actualizarUsuario,
+  borrarUsuario,
+};
